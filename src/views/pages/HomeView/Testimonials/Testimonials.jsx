e1981a3ed3eea9bf12e12e80fd416389e683f51f
Deepayan <deepayanmukherjee1312@gmail.com>
2021-09-28 10:32:11 +0530

    Added testimonials section along with carousel

--- src/views/pages/HomeView/Testimonials/Testimonials.jsx
+++ src/views/pages/HomeView/Testimonials/Testimonials.jsx
@@ -1,11 +1,41 @@
 import React from 'react'
+import Carousel from "react-elastic-carousel";
 import './Testimonials.css';
 
+import {data} from './data'
+
+const breakPoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 550, itemsToShow: 2, itemsToScroll: 2 },
+    { width: 768, itemsToShow: 3 },
+    { width: 1200, itemsToShow: 4 }
+  ];
+
 const Testimonials = () => {
+
+    
     return (
         <div className='testimonials'>
             <div className='test-heading'>
                 <h1>WHAT COMMUNITY SAYS ABOUT US?</h1>
+            </div>
+
+            <div className='test-comments'>
+                <Carousel breakPoints={breakPoints}>
+                    {data.map((item)=>(
+                        item.map((testimonial)=>(
+                            <div className='test-single'>
+                                <p>{testimonial.message}</p>
+                                <h3>{testimonial.name}</h3>
+                                <h5>{testimonial.position}</h5>
+                            </div>
+                        ))
+                    ))}
+                </Carousel>
+            </div>
+            <div className='test-pagination'>
+                <div className='pagemark'></div>
+                
             </div>
         </div>
     )
---
+++ src/views/pages/HomeView/Testimonials/data.js
@@ -0,0 +1,89 @@
+export const data=[[
+    {
+        message:
+          `Never thought programming would be that easy and interesting. Just watched the binary search video of Kunal and literally understood every single thing. It's such a boon to learn from you. Thnak you so much for providing this help`,
+        img: '',
+        name: 'Vasihnavi Bholane',
+        position: 'Student'
+      },
+      {
+        message:
+          `I have learned Java in my second year and none of my professors have taught what Kunal has taught. I have learned the memory management in stack and heap memory`,
+        img: '',
+        name: 'mrr.ayman',
+        position: 'Student'
+      },
+      {
+        message:
+          `I am following Community Classroom's bootcamp and found it really useful. I am getting all the concepts taught so far crystal clear as well as able to solve leetcode questions given in the assignments. Before this I was finding leetcode very difficult.`,
+        img: '',
+        name: 'Anurag Pathak',
+        position: 'Student'
+      }],
+      [
+        {
+            message:
+              `Never thought programming would be that easy and interesting. Just watched the binary search video of Kunal and literally understood every single thing. It's such a boon to learn from you. Thnak you so much for providing this help`,
+            img: '',
+            name: 'Vasihnavi Bholane',
+            position: 'Student'
+          },
+          {
+            message:
+              `I have learned Java in my second year and none of my professors have taught what Kunal has taught. I have learned the memory management in stack and heap memory`,
+            img: '',
+            name: 'mrr.ayman',
+            position: 'Student'
+          },
+          {
+            message:
+              `I am following Community Classroom's bootcamp and found it really useful. I am getting all the concepts taught so far crystal clear as well as able to solve leetcode questions given in the assignments. Before this I was finding leetcode very difficult.`,
+            img: '',
+            name: 'Anurag Pathak',
+            position: 'Student'
+          }],
+          [
+            {
+                message:
+                  `Never thought programming would be that easy and interesting. Just watched the binary search video of Kunal and literally understood every single thing. It's such a boon to learn from you. Thnak you so much for providing this help`,
+                img: '',
+                name: 'Vasihnavi Bholane',
+                position: 'Student'
+              },
+              {
+                message:
+                  `I have learned Java in my second year and none of my professors have taught what Kunal has taught. I have learned the memory management in stack and heap memory`,
+                img: '',
+                name: 'mrr.ayman',
+                position: 'Student'
+              },
+              {
+                message:
+                  `I am following Community Classroom's bootcamp and found it really useful. I am getting all the concepts taught so far crystal clear as well as able to solve leetcode questions given in the assignments. Before this I was finding leetcode very difficult.`,
+                img: '',
+                name: 'Anurag Pathak',
+                position: 'Student'
+              }],
+              [
+                {
+                    message:
+                      `Never thought programming would be that easy and interesting. Just watched the binary search video of Kunal and literally understood every single thing. It's such a boon to learn from you. Thnak you so much for providing this help`,
+                    img: '',
+                    name: 'Vasihnavi Bholane',
+                    position: 'Student'
+                  },
+                  {
+                    message:
+                      `I have learned Java in my second year and none of my professors have taught what Kunal has taught. I have learned the memory management in stack and heap memory`,
+                    img: '',
+                    name: 'mrr.ayman',
+                    position: 'Student'
+                  },
+                  {
+                    message:
+                      `I am following Community Classroom's bootcamp and found it really useful. I am getting all the concepts taught so far crystal clear as well as able to solve leetcode questions given in the assignments. Before this I was finding leetcode very difficult.`,
+                    img: '',
+                    name: 'Anurag Pathak',
+                    position: 'Student'
+                  }],
+]
