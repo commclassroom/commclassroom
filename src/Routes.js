d4b87bde50076d34d88947b8e750181314cb3f8f
NeeteshKing21 <neeteshking21@gmail.com>
2021-10-04 22:59:01 +0530

    Add course hero templete

--- src/Routes.js
+++ src/Routes.js
@@ -3,7 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 // import LoadingScreen from './components/LoadingScreen';
 import HomeView from './views/pages/HomeView/HomeView';
 import MainLayout from './layouts/MainLayout';
-import Hero from './views/pages/CoursesView/Hero'
+import Hero from './views/pages/CoursesView/Hero';
 // feel free to create own routes & just rock on guys .
 const Routes = () => {
   return (
