8bf7a7513df4f466269310fe6c0613968375e3c7
lakshaycode22 <lakshaycode22@gmail.com>
2021-10-23 21:35:03 +0530

    Changes 1 (course page)

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,45 +1,39 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-//import icons
-import { BsArrowLeftCircle } from 'react-icons/bs';
-import { BsArrowRightCircle } from 'react-icons/bs';
-import balloon from '../../assets/images/ballon.png';
-
-//import banners
-import dsa from '../../assets/static/react-web-designs/course_banners/light-theme/dsa.png';
-import webdev from '../../assets/static/react-web-designs/course_banners/light-theme/webdev.png';
-import opensource from '../../assets/static/react-web-designs/course_banners/light-theme/opensource.png';
-import devops from '../../assets/static/react-web-designs/course_banners/light-theme/devops.png';
-
-const course = [
-  {
-    key: 0,
-    title: 'Data Structures and Algorithms',
-    image: dsa
-  },
-  {
-    key: 1,
-    title: 'Web Development',
-    image: webdev
-  },
-  {
-    key: 2,
-    title: 'Open Source',
-    image: opensource
-  },
-  {
-    key: 3,
-    title: 'DevOps',
-    image: devops
-  }
-];
+import prevButton from '../../assets/static/arrowLeft.svg';
+import nextButton from '../../assets/static/arrowRight.svg';
+import balloon from '../../assets/static/react-web-designs/ballon.svg';
+import blob from '../../assets/static/react-web-designs/courses_blob.svg';
+import { courses } from '../../data/Courses/course';
 
 const Courses = () => {
   const [value, setValue] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const [course, setCourse] = useState([]);
+
+  useEffect(() => {
+    setCourse(courses);
+    setLoading(false);
+  })
+
+  if(loading == true){
+    return(
+      <div>
+        Loading...
+      </div>
+    )
+  }
+  
   return (
     <div>
-      <div className="absolute left-0 top-80 transform scale-125 ">
+      {/* balloon */}
+      <div className="absolute left-0 top-80">
         <img src={balloon} alt="balloon" />
+      </div>
+
+      {/* blob */}
+      <div className="absolute -right-60 -bottom-24 transform scale-125">
+        <img src={blob} />
       </div>
 
       <h2 className="text-xl mt-24 mx-40 text-secondary font-bold filter drop-shadow-xl">
@@ -47,12 +41,15 @@ const Courses = () => {
       </h2>
 
       <div className="my-6 mx-40 flex">
-        <div className="">
+        <div className="mb-16 mr-32">
+          {/* heading */}
           <h1 className="text-4xl font-black">
             Courses that are
             <br />
-            <span className="text-secondary">custom</span> tailered for you
+            <span className="text-secondary">custom </span>tailered for you
           </h1>
+
+          {/* list of courses */}
           <div className="font-bold text-xl mt-24 leading-10 text-third">
             {course.map((course) => (
               <ul key={course.key} onClick={() => setValue(course.key)}>
@@ -67,25 +64,32 @@ const Courses = () => {
             ))}
           </div>
         </div>
-        <div className="">
+
+        {/* image */}
+        <div className="flex-1">
           <img
-            className="rounded-2xl object-center"
+            className="rounded-2xl transform scale-125"
             src={course[value].image}
             alt={course[value].title}
           />
         </div>
       </div>
 
-      <div className="flex mx-40 mb-40 text-center">
-        <div className="flex-1">
-          <button className="text-sm border-none py-1.5 px-5 mr-3 bg-secondary text-fourth rounded-md">
+      <div className="flex mx-40 mb-40 text-center ">
+        {/* get started button */}
+        <div className="flex-1 filter drop-shadow-xl">
+          <button className="text-sm border-none py-1.5 px-5 mr-3 bg-secondary text-fourth rounded-md"
+          onClick={() => {
+            location.href = course[value].link;
+          }}>
             Get Started
           </button>
         </div>
 
+        {/* bullets */}
         <div className="mx-40 flex">
           {course.map((course) => (
-            <div key={course.key} className="text-third flex mx-6 text-xl">
+            <div key={course.key} className="text-third flex mx-4 text-xl">
               <span className={`${course.key === value && 'text-primary'}`}>
                 &#9632;
               </span>
@@ -93,29 +97,22 @@ const Courses = () => {
           ))}
         </div>
 
-        <div className="flex flex-1 text-3xl text-primary">
+        {/* prev next buttons */}
+        <div className="flex flex-1 transform scale-150 z-50 filter drop-shadow-xl">
           <div
-            className="ml-24 mr-4"
+            className="ml-24 "
             onClick={() => {
-              if (value === 0) {
-                setValue(course.length - 1);
-              } else {
-                setValue(value - 1);
-              }
+              value === 0 ? setValue(course.length - 1) : setValue(value - 1);
             }}
           >
-            <BsArrowLeftCircle />
+            <img src={prevButton} />
           </div>
           <div
             onClick={() => {
-              if (value === course.length - 1) {
-                setValue(0);
-              } else {
-                setValue(value + 1);
-              }
+              value === course.length - 1 ? setValue(0) : setValue(value + 1);
             }}
           >
-            <BsArrowRightCircle />
+            <img src={nextButton} />
           </div>
         </div>
       </div>
