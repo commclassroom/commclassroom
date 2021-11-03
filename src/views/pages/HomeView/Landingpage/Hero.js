f06fb46eefb616c22a10165c91d16c90acb6919c
AbhinavMV <mvabhinav1998@gmail.com>
2021-11-04 01:40:30 +0530

    Added Navbar Functionality

---
+++ src/views/pages/HomeView/Landingpage/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Page from '../../../../components/Hero/index';
+
+const Hero = () => {
+  return (
+    // Main div
+    <div>
+      <div className="flex flex-row font-redHatDisplay">
+        <Page />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
