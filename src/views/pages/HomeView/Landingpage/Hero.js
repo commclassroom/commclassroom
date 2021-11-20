3bce748519fe8bcefb87dc6518075b104d024ca5
AbhinavMV <mvabhinav1998@gmail.com>
2021-11-20 20:19:57 +0530

    Add scroll effect for home

--- src/views/pages/HomeView/Landingpage/Hero.js
+++ src/views/pages/HomeView/Landingpage/Hero.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import { Element } from 'react-scroll';
+
 import Page from '../../../../components/Hero/index';
 
 const Hero = () => {
   return (
     // Main div
-    <div>
+    <Element name="home" className="element">
       <div className="flex flex-row font-redHatDisplay">
         <Page />
       </div>
-    </div>
+    </Element>
   );
 };
 
