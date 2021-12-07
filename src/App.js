14c62260062a450472b62c0c2671baff275cecd4
IsomerX <49413460+IsomerX@users.noreply.github.com>
2021-12-08 01:28:39 +0530

    added the theme toggle switch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { ThemeContext } from './store/context';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 //import { createStyles, makeStyles } from '@material-ui/core';
@@ -7,12 +8,19 @@ import './App.css';
 const history = createBrowserHistory();
 
 function App() {
+  const [theme, setTheme] = useState('light');
+
   return (
-    <>
+    <ThemeContext.Provider
+      value={{
+        theme,
+        setTheme
+      }}
+    >
       <Router history={history}>
         <Routes />
       </Router>
-    </>
+    </ThemeContext.Provider>
   );
 }
 
